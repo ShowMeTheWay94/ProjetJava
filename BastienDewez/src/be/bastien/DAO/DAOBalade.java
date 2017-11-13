3d9aa01d80ebdd98ddc263cd025ae392fad7879b
bastien <bastiendewez@hotmail.com>
2017-11-13 21:46:41 +0100

    Amélioration remboursement des chauffeurs

--- BastienDewez/src/be/bastien/DAO/DAOBalade.java
+++ BastienDewez/src/be/bastien/DAO/DAOBalade.java
@@ -38,16 +38,16 @@ public class DAOBalade extends DAO<Balade> {
 		try {
 			ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY)
 					.executeQuery("SELECT * FROM BALADE WHERE IDBALADE = " + idBalade);
-			if(result.first()) {
+			while(result.next()) {
 				balade.setIdBalade(result.getInt("IDBALADE"));
 				balade.setNomBalade(result.getString("NOMBALADE"));
 				balade.setLieuDepart(result.getString("LIEU"));
 				balade.setDateBalade(result.getDate("DATEBALADE"));
 				balade.setForfait(result.getDouble("FORFAIT"));
-				result = this.connect.createStatement().executeQuery("SELECT NUMIMMATRICULATION FROM BALADE_VEHICULE BV INNER JOIN"
+				ResultSet result1 = this.connect.createStatement().executeQuery("SELECT NUMIMMATRICULATION FROM BALADE_VEHICULE BV INNER JOIN"
 						+ " BALADE B ON BV.IDBALADE = B.IDBALADE WHERE BV.IDBALADE = " + balade.getIdBalade());
-				while(result.next()) {
-					Vehicule vehicule = daoVehicule.find(result.getString("NUMIMMATRICULATION"));
+				while(result1.next()) {
+					Vehicule vehicule = daoVehicule.find(result1.getString("NUMIMMATRICULATION"));
 					listeVehicule.add(vehicule);
 				}	
 				balade.setListVehicule(listeVehicule);
@@ -58,5 +58,37 @@ public class DAOBalade extends DAO<Balade> {
 		}
 		
 		return balade;
+	}
+	
+	public int nbrBalade() {
+		int nbr = 0;
+		
+		try {
+			ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT COUNT(*) NBR FROM BALADE");
+			if(result.first()) {
+				nbr = result.getInt("NBR");
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return nbr;
+	}
+	
+	public int nbrBaladeDifferente(int idBalade) {
+		int nbr = 0;
+		
+		try {
+			ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT COUNT(" + idBalade + ") NBR FROM BALADE_VEHICULE");
+			if(result.first()) {
+				nbr = result.getInt("NBR");
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return nbr;
 	}
 }
